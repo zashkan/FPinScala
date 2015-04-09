6bdde9a009420de769e75cb3d64de16c786bc09b
Yawar Amin <yawar.amin@yp.ca>
2015-04-09 15:57:51 -0400

    datastructures: test Exercise 3.1

---
+++ exercises/src/test/scala/datastructures/ListSpec.scala
@@ -0,0 +1,11 @@
+import org.specs2.mutable.Specification
+import fpinscala.datastructures._
+
+class ListSpec extends Specification {
+  "Exercise 3.1" >> {
+    "x must be 3" >> {
+      List.x mustEqual 3
+    }
+  }
+}
+
