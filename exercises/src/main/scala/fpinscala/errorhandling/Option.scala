e644f7f2e985d35d92af87436a812ee71356142d
Yawar Amin <yawar.amin@mediative.com>
2015-04-16 15:03:37 -0400

    errorhandling: implement and test the Option functions

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,18 +1,27 @@
 package fpinscala.errorhandling
 
-
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case Some(a) => Some(f(a))
+      case None => None
+    }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case Some(a) => a
+      case None => default
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] =
+    flatMap { a => if (f(a)) Some(a) else None }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
---
+++ exercises/src/test/scala/errorhandling/OptionSpec.scala
@@ -0,0 +1,68 @@
+import org.specs2.mutable.Specification
+
+import fpinscala.errorhandling._
+
+class OptionSpec extends Specification {
+  val someObj = Some(1)
+  val someOtherObj = Some(2)
+  def add1(i: Int) = i + 1
+  def addSome1(i: Int) = Some(add1(i))
+
+  "map" should {
+    "map a value using the provided function" in {
+      someObj.map(add1) mustEqual someOtherObj
+    }
+
+    "map an absent value to an absent value" in {
+      None.map(add1) mustEqual None
+    }
+  }
+
+  "getOrElse" should {
+    "get the value out of a Some object" in {
+      someObj.getOrElse(2) mustEqual 1
+    }
+
+    "get the default value if given a None object" in {
+      None.getOrElse(2) mustEqual 2
+    }
+  }
+
+  "flatMap" should {
+    "map and then flatten" in {
+      someObj.flatMap(addSome1) mustEqual someOtherObj
+    }
+
+    "map an absent value" in {
+      None.flatMap(addSome1) mustEqual None
+    }
+
+    "map to a function that returns an absent value" in {
+      someObj.flatMap(_ => None) mustEqual None
+    }
+  }
+
+  "orElse" should {
+    "return the optional if it contains a value" in {
+      someObj.orElse(someOtherObj) mustEqual someObj
+    }
+
+    "return the default if the optional is missing a value" in {
+      None.orElse(someOtherObj) mustEqual someOtherObj
+    }
+  }
+
+  "filter" should {
+    "preserve the optional value if it meets the predicate condition" in {
+      someObj.filter(_ == 1) mustEqual someObj
+    }
+
+    "drop the optional value if it fails the predicate condition" in {
+      someObj.filter(_ != 1) mustEqual None
+    }
+
+    "pass through an absent optional value" in {
+      None.filter(_ == 1) mustEqual None
+    }
+  }
+}
