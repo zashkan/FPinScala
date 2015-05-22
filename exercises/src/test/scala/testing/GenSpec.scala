6a9147af4103a7a91d2d38b7890426dfd49bd8b3
Yawar Amin <yawar.amin@mediative.com>
2015-05-22 15:32:28 -0400

    MPN-1004: Exercise 8.1
    List out some properties to test an implementation of sum: List[Int] =>
    Int.

---
+++ exercises/src/test/scala/testing/GenSpec.scala
@@ -0,0 +1,15 @@
+/*
+Ex. 8.1
+
+Some properties (expressed as functions) to test an implementation of
+sum: List[Int] => Int
+
+  1. xs: List[Int] => sum(xs) == sum(xs.reverse)
+
+  2. n: Int => x: Int => sum(List.fill(n)(x)) == n * x
+
+  3. sum(List.empty) == 0
+
+  4. x: Int => sum(List(x)) == x
+*/
+
