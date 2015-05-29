4718331e1a82fcccd140118fbc6d9c9ae69d6551
Yawar Amin <yawar.amin@mediative.com>
2015-05-29 15:49:12 -0400

    MPN-1004: Exercise 8.2
    List some property tests that specify the list maximum function.

--- exercises/src/test/scala/testing/GenSpec.scala
+++ exercises/src/test/scala/testing/GenSpec.scala
@@ -11,5 +11,15 @@ sum: List[Int] => Int
   3. sum(List.empty) == 0
 
   4. x: Int => sum(List(x)) == x
+
+Ex. 8.2
+
+Properties that specify an implementation of maximum: List[Int] => Int
+
+  1. x: Int => maximum(List(x)) == x
+
+  2. n: Int => x: Int => maximum(List.fill(n)(x)) == x
+
+  3. xs: List[Int] => maximum(xs) == maximum(xs.reverse)
 */
 
