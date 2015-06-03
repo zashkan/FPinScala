7985111d12dcaab6568e08a64507518c83ce8b65
Ashkan Zarnani <azarnan1@yps-mac-mini.ad.ypg.com>
2015-06-02 17:43:01 -0700

    working on 4.1

---
+++ exercises/src/test/scala/errorhandling/EitherSpec.scala
@@ -0,0 +1,16 @@
+import org.specs2.mutable.Specification
+import org.specs2.ScalaCheck
+
+import fpinscala.errorhandling._
+
+class erroSpec extends Specification {
+ //========================================================================================
+  //#3.2
+  "error" should {
+    "return Nil for an empty List" in {
+      1 mustEqual 1
+    }
+
+  }
+
+}
---
+++ exercises/src/test/scala/errorhandling/OptionSpec.scala
@@ -0,0 +1,1 @@
+import fpinscala.errorhandling._
