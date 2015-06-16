97697a93efd290b2842486acd83ad3c40713a9d6
ashkan <ashkan.zarnani@mediative.com>
2015-06-15 20:50:27 -0700

    implementation only done until 6.6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -19,7 +19,7 @@ object RNG {
 
   type Rand[+A] = RNG => (A, RNG)
 
-  val int: Rand[Int] = _.nextInt
+  //val int: Rand[Int] = _.nextInt
 
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
@@ -30,19 +30,72 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    ((i.toDouble-1)/Int.MaxValue, r)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def double_via_map: Rand[Double] =
+    map(nonNegativeInt)(i => (i.toDouble-1)/Int.MaxValue)
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, rng2) = rng.nextInt
+    val (d, rng3) = double(rng2)
+    ((i,d), rng3)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i,d), rng2) = intDouble(rng)
+    ((d,i), rng2)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    ((d1,d2,d3), rng4)
+  }
+
+  // def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+  //   var rOld = rng
+  //   var rNew = rng
+  //   var xs:List[Int] = List()
+  //   //var x = 0
+
+  //   for(i <- 1 to count) {
+  //     var (x,rNew) = rOld.nextInt
+  //     xs = x +: xs
+  //     println(i)
+  //     rOld = rNew
+  //   }
+  //   (xs, rNew)
+  // }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @annotation.tailrec
+    def go(c: Int, acc: List[Int])(rng: RNG): (List[Int], RNG) = {
+      if (c < 1) (acc, rng)
+      else {
+        val (i, r) = rng.nextInt
+        go(c-1, i::acc)(r)
+      }
+    }
+    go(count, List())(rng)
+  }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rngA) = ra(rng)
+      val (b, rngB) = rb(rngA)
+      (f(a, b), rngB)
+    }
+  }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
@@ -67,4 +120,38 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object State {
   type Rand[A] = State[RNG, A]
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+}
+
+object myModule {
+  def main(args: Array[String]) = {
+    println("from state!")
+    var rng = RNG.Simple(42)
+    println(rng)
+    println(rng.nextInt)
+    println(rng.nextInt)
+
+    // var r = rng.nextInt
+    // while (r._1 > 0) {
+    //   rng = r._2
+    //   r = rng.nextInt
+    // }
+    // println(r)
+    println(RNG.nonNegativeInt(rng))
+
+    println(Int.MaxValue, Int.MinValue)
+    val (i, rng2) = RNG.double(rng)
+
+    println("==========================")
+    println("double:")
+    println(RNG.double(rng2))
+    println(RNG.double_via_map(rng2))
+
+    println("==========================")
+    println(rng2.nextInt)
+    println(RNG.intDouble(rng2))
+    println(RNG.doubleInt(rng2))
+    println(RNG.double3(rng2))
+
+    println(RNG.ints(3)(rng2))
+  }
 }
